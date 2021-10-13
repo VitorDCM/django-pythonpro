20fa7fac1152ddb90a6a0af294e51cbbfcfad47e
Vitor Dantas <vitordcmorais@gmail.com>
2021-10-13 10:06:37 -0300

    Correção de configuração para variável DEBUG e adição da SECRET_KEY.

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 from decouple import config
 
 
@@ -22,14 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
