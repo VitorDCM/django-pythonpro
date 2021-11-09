3fdac8d18bb0fc61a4d65fa1defd1572c299582f
Vitor Dantas <vitordcmorais@gmail.com>
2021-11-08 21:29:30 -0300

    Adição da estratégia de armazenagem da collectfast

--- pypro/settings.py
+++ pypro/settings.py
@@ -35,6 +35,8 @@ AUTH_USER_MODEL = 'base.User'
 
 # Application definition
 
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -145,7 +147,7 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 if AWS_ACCESS_KEY_ID:
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_OBJECT_PARAMETERS = { 'CacheControl': 'max-age-86400', }
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age-86400', }
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
