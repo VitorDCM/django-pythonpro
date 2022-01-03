f3eaae1dd473cf918002a841723dad70c9ac3e31
Vitor Dantas <vitordcmorais@gmail.com>
2022-01-03 16:14:22 -0300

    Criação da página de detalhes das aulas.

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,11 +1,11 @@
 from typing import List
 
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 
 
 def listar_modulos_ordenados() -> List[Modulo]:
     """
-    Lista módulos ordenados por títulos
+    Lista modulos ordenados por titulos.
     :return:
     """
 
@@ -13,4 +13,31 @@ def listar_modulos_ordenados() -> List[Modulo]:
 
 
 def encontrar_modulo(slug: str) -> Modulo:
+    """
+    Retorna um objeto do tipo Modulo a partir do seu slug.
+
+    @param slug:
+    @return: Modulo
+    """
     return Modulo.objects.get(slug=slug)
+
+
+def listar_aulas_de_modulo_ordenadas(modulo: Modulo) -> List[Modulo]:
+    """
+    Retorna uma lista de modulos que contem aulas.
+
+    @param modulo:
+    @return:list[Modulo]
+    """
+
+    return list(modulo.aula_set.order_by('order').all())
+
+
+def encontrar_aula(slug: str) -> Aula:
+    """
+    Retorna um objeto do tipo Aula a partir do seu slug.
+
+    @param slug:
+    @return: Aula
+    """
+    return Aula.objects.get(slug=slug)
